89d1938414ddd87316bcf81cd3ce3f2a6819a749
asif <mhamudasif2@gmail.com>
2022-12-09 16:31:51 +0600

    :)

--- components/iconButton.js
+++ components/iconButton.js
@@ -1,11 +1,9 @@
-import Link from "next/Link";
+import Link from "next/link";
 
 const IconButton = ({ url, children }) => {
   return (
     <div className="mr-7 hover:text-white">
-      <Link href={url}>
-        {children}
-      </Link>
+      <Link href={url}>{children}</Link>
     </div>
   );
 };
