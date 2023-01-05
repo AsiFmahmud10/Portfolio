05abac1d07188fa0796cc4cd0a095d807ffae31d
asif <mhamudasif2@gmail.com>
2023-01-05 16:45:58 +0600

    ..

--- components/card.js
+++ components/card.js
@@ -38,7 +38,7 @@ const Card = ({data,link,width,height}) => {
                      <div className="text-md text-center h-[111px;]">
                            {
                               tech.map((items)=>(
-                                 <button className=" skill-card-btn " key={items}>
+                                 <button className=" skill-card-btn dark:hover:text-orange-300 " key={items}>
                                     # {items}
                                  </button>     
                                  )
@@ -52,10 +52,10 @@ const Card = ({data,link,width,height}) => {
                               buttonIcons.map((Btn,index)=>(
                               <Link key={index} href={ Btn?.link ? Btn.link :'' }>
                                  <div className="group">
-                                    <button  className=" relative skill-card-Iconbtn">
+                                    <button  className=" relative dark:hover:text-orange-300 skill-card-Iconbtn">
                                        <Btn.icon size={26}/>
                                     </button>
-                                    <div className="hidden absolute text-red-500 group-hover:block text-center pl-6 pr-5 ">{Btn.name}</div>
+                                    <div className="hidden absolute dark:text-white text-red-500 group-hover:block text-center pl-6 pr-5 ">{Btn.name}</div>
                                  </div>
                               </Link> 
                               ))    
