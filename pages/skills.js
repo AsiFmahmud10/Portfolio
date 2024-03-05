e70096f7bd9fd7e01d488dba4799223c500f4c31
asif <mhamudasif2@gmail.com>
2024-03-05 18:22:21 +0600

    project page

--- pages/skills.js
+++ pages/skills.js
@@ -2,7 +2,7 @@ import { ProjectsGrid } from "../components";
 
 const SkillPage = () => {
   return (
-    <div className="w-screen  h-screen dark:bg-gradient-to-r from-start to-blue-900">
+    <div className="w-screen  h-full dark:bg-gradient-to-r from-start to-blue-900 pb-3 ">
       <div className=""> </div>
       
       <ProjectsGrid/>
